3867be8b3c8ec4e2d280793b3aac251008b670ce
HappyYyh <863044052@qq.com>
2018-06-14 22:24:27 +0800

    中转信息完成

---
+++ src/main/java/com/njxzc/wlgl/service/transfer/ZzgsxxService.java
@@ -0,0 +1,24 @@
+package com.njxzc.wlgl.service.Transfer;
+
+import com.njxzc.wlgl.pojo.Transfer.Zzgsxx;
+
+import java.util.ArrayList;
+
+public interface ZzgsxxService {
+
+    void deleteByPrimaryKey(Integer id);
+
+    //增加中转公司信息
+    int insert(Zzgsxx zzgsxx);
+    //查询中转公司信息
+    ArrayList<Zzgsxx> queryZzgsxx(Zzgsxx zzgsxx);
+
+    int updateByPrimaryKey(Zzgsxx zzgsxx);
+   /* int insertSelective(com.njxzc.wlgl.pojo.Transfer.Zzgsxx record);
+
+    com.njxzc.wlgl.pojo.Transfer.Zzgsxx selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(com.njxzc.wlgl.pojo.Transfer.Zzgsxx record);
+
+    int updateByPrimaryKey(com.njxzc.wlgl.pojo.Transfer.Zzgsxx record);*/
+}
